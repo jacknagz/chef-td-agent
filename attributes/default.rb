0b493f9cca5aff5134d8c5fb1a4c11c0b851c30d
Jack Naglieri <jack.naglieri@airbnb.com>
2017-05-15 14:33:11 -0700

    [configuration] Add new options for internal repos, and disabling the API usage

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,26 +1,29 @@
-default["td_agent"]["api_key"] = ''
+default['td_agent']['enable_api'] = false
+default['td_agent']['api_key'] = ''
 
-default["td_agent"]["plugins"] = []
+default['td_agent']['plugins'] = []
 
-default["td_agent"]["uid"] = nil
-default["td_agent"]["gid"] = nil
+default['td_agent']['uid'] = nil
+default['td_agent']['gid'] = nil
 
-default["td_agent"]["user"] = 'td-agent'
-default["td_agent"]["group"] = 'td-agent'
+default['td_agent']['user'] = 'td-agent'
+default['td_agent']['group'] = 'td-agent'
 
-default["td_agent"]["includes"] = false
-default["td_agent"]["default_config"] = true
-default["td_agent"]["template_cookbook"] = 'td-agent'
-default["td_agent"]["in_http"]["enable_api"] = true
-default["td_agent"]["version"] = "2.2.0"
-default["td_agent"]["pinning_version"] = false
-default["td_agent"]["apt_arch"] = 'amd64'
-default["td_agent"]["in_forward"] = {
-  port: 24224,
+default['td_agent']['version'] = '2.3.4'
+default['td_agent']['internal_repo'] = false
+default['td_agent']['pinning_version'] = false
+default['td_agent']['apt_arch'] = 'amd64'
+
+default['td_agent']['includes'] = false
+default['td_agent']['default_config'] = true
+default['td_agent']['template_cookbook'] = 'td-agent'
+default['td_agent']['in_http']['enable_api'] = true
+default['td_agent']['in_forward'] = {
+  port: 24_224,
   bind: '0.0.0.0'
 }
-default["td_agent"]["in_http"] = {
+default['td_agent']['in_http'] = {
   port: 8888,
   bind: '0.0.0.0'
 }
-default["td_agent"]["yum_amazon_releasever"] = "$releasever"
+default['td_agent']['yum_amazon_releasever'] = '$releasever'
